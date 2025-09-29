3f7c26e2f1abe164e35612732205c634e1da9f24
septa <septa.azisaputra@ai.astra.co.id>
2025-09-29 22:05:52 +0700

    fix(server): fix prisma client import

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client/extension";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "prisma/generated/client";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
